d162905654f9d67e3d6aee6d86439d5ccc0d38cf
GreyWolfCab <54644740+GreyWolfCab@users.noreply.github.com>
2020-11-22 17:04:40 -0800

    Added comments

--- UDPServer.java
+++ UDPServer.java
@@ -5,17 +5,17 @@ public class UDPServer {
 
     public static void main(String[] args) {
 
-        try (DatagramSocket aSocket = new DatagramSocket(6789)) {
-            byte[] buffer = new byte[1000];
+        try (DatagramSocket aSocket = new DatagramSocket(6789)) {//initialize the socket with a hardcoded port number
+            byte[] buffer = new byte[1000];//will store the incoming request
 
-            while (true) {
+            while (true) {//infinitly listen for requests
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                aSocket.receive(request);
+                aSocket.receive(request);//store incoming request
 
                 DatagramPacket reply = new DatagramPacket(request.getData(),
-                        request.getLength(), request.getAddress(), request.getPort());
+                        request.getLength(), request.getAddress(), request.getPort());//build a reply based on the request received
 
-                aSocket.send(reply);
+                aSocket.send(reply);//send response to client
             }
         } catch (SocketException e) {
             System.out.println("Socket: " + e.getMessage());
